795f693f0e53c47c4ca755be4fa8fdd1de74cf2f
ygchan <ygchan89@gmail.com>
2019-09-21 22:26:15 -0400

    Mac OS uses command key

--- Citibike_ETL.py
+++ Citibike_ETL.py
@@ -6,28 +6,45 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+import time
+
 # Reference: https://stackoverflow.com/a/49851826
 # This is the only way George able to make Chrome() to work on my laptop.
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 citibike_website = 'https://s3.amazonaws.com/tripdata/index.html'
 
-title = driver.title
-
 # Open a browser and visit the website
 driver.get(citibike_website)
-wait(driver, 15).until_not(EC.title_is(title))
-print('Page Loaded!')
+title = driver.title
+# wait(driver, 15).until_not(EC.title_is(title))
+print(driver.title)
 
-actions = ActionChains(driver)
-actions.key_down(Keys.CONTROL)
-actions.send_keys('a')
-actions.key_up(Keys.CONTROL)
-actions.perform()
+time.sleep(10)
 
-actions.key_down(Keys.CONTROL)
-actions.send_keys('c')
-actions.key_up(Keys.CONTROL)
-actions.perform()
+# Click the title once
+driver.find_element_by_name('h1-title').click()
 
+# Send the command A
+ActionChains(driver).key_down(Keys.COMMAND)
+time.sleep(2)
+ActionChains(driver).send_keys('a')
+time.sleep(2)
+ActionChains(driver).key_up(Keys.COMMAND)
+time.sleep(2)
+ActionChains(driver).perform()
+
+print('Send Command A...')
+
+# Send the command C
+time.sleep(2)
+ActionChains(driver).key_down(Keys.COMMAND)
+time.sleep(2)
+ActionChains(driver).send_keys('c')
+time.sleep(2)
+ActionChains(driver).key_up(Keys.COMMAND)
+time.sleep(2)
+ActionChains(driver).perform()
+
+print('Send Command C...')
 print('Done!')
